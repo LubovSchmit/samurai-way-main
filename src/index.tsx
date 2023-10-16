6c646a48ffd23db78fa121ac7b13ce5e2ccd3c3e
LiubovSchmit <liubou.schmit@gmail.com>
2023-10-16 13:41:48 +0200

    HashRouter using instead BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import App from './App';
 import {store} from './redux/reduxStore/reduxStore';
 import {Provider} from 'react-redux';
 
 
 ReactDOM.render(
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter basename={process.env.PUBLIC_URL}>
             <Provider store={store}>
                 <App />
             </Provider>
-        </BrowserRouter>,
+        </HashRouter>,
         document.getElementById('root'));
 
 
